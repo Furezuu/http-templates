5fffb2d1533828ba8fcda63c09be6584a4c44156
VoRonex <nikatguy@gmail.com>
2020-03-11 11:16:21 +0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask('my server')
+
+
+@app.route('/index/<title>')
+@app.route('/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    params = {}
+    params['prof'] = prof
+    return render_template('training.html', **params)
+
+
+if __name__ == '__main__':
+    app.run('localhost', 8080)
